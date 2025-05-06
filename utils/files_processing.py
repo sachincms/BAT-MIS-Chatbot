e6264857f4df3fde1a5f72674e9e3b36a0bd9747
sahilgk-cms <sahil@catalysts.org>
2025-05-06 18:07:27 +0530

    updated code for files processing to store data in json rather than text

--- utils/files_processing.py
+++ utils/files_processing.py
@@ -1,96 +1,87 @@
 from typing import List, Tuple
-from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from llama_index.core import  Document
 from typing import List
 import os
-import llama_index
 import pandas as pd
 import json
 from pypdf import PdfReader
 from datetime import datetime
 from dotenv import load_dotenv
 
+# Removed the old code
+# In the chat_app we will use the bat.json file rather than bat_pdf_text.txt file
 
-############################################## LLAMAINDEX DOCUMENTS ################################################################
-
-def convert_pdf_files_into_llamaindex_docs(folder_path: str,
-                                           chunk_size: int,
-                                           chunk_overlap: int) -> List[llama_index.core.schema.Document]:
-    '''
-  This function converts the pdfs present in the folder path into llamaindex documents.
-  Args:
-    pdf_folder_path, chunk_size, chunk_overlap
-  Returns:
-    list of llamaindex documents
-   '''
-    pdf_files = [file for file in os.listdir(folder_path) if file.endswith(".pdf")]
-
-    langchain_documents = []
-    for pdf_file in pdf_files:
-        pdf_file_path = os.path.join(folder_path, pdf_file)
-        pdf_loader = PyPDFLoader(pdf_file_path)
-        pdf_documents = pdf_loader.load()
-        langchain_documents.extend(pdf_documents)
-
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = chunk_size, chunk_overlap = chunk_overlap)
-    split_documents = text_splitter.split(langchain_documents)
-
-    llamaindex_documents = []
-    for doc in split_documents:
-        llamaindex_doc = Document.from_langchain_format(doc)
-        llamaindex_documents.append(llamaindex_doc)
-    
-    return llamaindex_documents
-
-
-################################################ PDF TO TEXT ################################################################################
-
-def convert_pdf_into_text(folder_path: str) -> str:
+######################################## PDF to JSON #################################################
+def convert_pdf_into_json(folder_path: str) -> dict:
   '''
-  This function converts all pdf data into text string
+  This function converts all pdf data into text string with pdf name as key and text as values
   Args:
     folder path containing all pdfs
   Returns:
-    text string
+    dict containing pdf name as key and text as values
   '''
+  final_dict = {}
   pdf_files = [file for file in os.listdir(folder_path) if file.endswith(".pdf")]
-  print(f"PDF Files in Google Drive Folder: {pdf_files}")
 
-  text = ""
   for pdf_file in pdf_files:
+    print(f"Currently working on {pdf_file}")
+    text = ""
     pdf_path = os.path.join(folder_path, pdf_file)
     reader = PdfReader(pdf_path)
 
     for page in reader.pages:
       text += page.extract_text()
 
-  return text
+    final_dict[pdf_file] = text
+
+  return final_dict
 
 
-def save_string_to_textfile(text: str, filepath: str):
+######################################## JSON processing #################################################
+
+def custom_serializer(obj):
+    """
+    Serialize non-JSON serializable objects, such as datetime.
+    Args:
+        obj (any): The object to serialize.
+    Returns:
+        str: An ISO 8601 formatted string if the object is a datetime instance.
+    Raises:
+        TypeError: If the object type is not supported for serialization.
+    """
+    if isinstance(obj, datetime):
+      return obj.isoformat()
+    raise TypeError(f"Type {type(obj)} is not serializable")
+
+
+
+def save_dict_to_json(input_dict: dict, file_path: str):
   '''
-  This function saves string to text file
+  This function saves the dictionary to JSON file
   Args:
-    string
+    input dictionary, file path
   Returns:
     None
   '''
-  with open(filepath, "w", encoding = "utf-8") as f:
-    f.write(text)
+  with open(file_path, "w") as f:
+    json.dump(input_dict, f, default=custom_serializer)
 
 
-def load_string_from_textfile(filepath: str) -> str:
+
+def load_dict_from_json(file_path: str) -> dict:
   '''
-  This function loads string from a text file
+  This function loads the dictionary from json file
   Args:
-    Text file path
+    file path
   Returns:
-    String
+    dictionary
   '''
-  with open(filepath, "r", encoding="utf-8") as f:
-    text = f.read()
-  return text
+  with open(file_path, "r") as f:
+    loaded_dict  = json.load(f)
+  return loaded_dict
+
+
+
+
 
 
 
