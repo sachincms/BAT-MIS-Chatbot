a83b836abe9885c281e46ffe770d136fc844c8c2
sachincms <sachin.r@catalysts.org>
2025-05-07 18:18:30 +0530

    removed user management page

--- navigation.py
+++ navigation.py
@@ -1,9 +1,9 @@
 import streamlit as st
+import os
 
 pages = [
-    st.Page("chat_app.py", title = "Chatbot"),
-    st.Page("situational_reports.py", title = "Situational Reports"),
-    st.Page("manage_users.py", title = "User Management")
+    st.Page(os.path.join("pages","chat_app.py"), title = "Chatbot"),
+    st.Page(os.path.join("pages", "situational_reports.py"), title = "Situational Reports"),
 ]
 
 pg = st.navigation(pages)
