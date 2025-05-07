82eacce7bb8453ac56411304502a7f6ffb312d1b
sahilgk-cms <sahil@catalysts.org>
2025-05-07 17:16:20 +0530

    removed manage users

--- navigation.py
+++ navigation.py
@@ -3,7 +3,7 @@ import streamlit as st
 pages = [
     st.Page("chat_app.py", title = "Chatbot"),
     st.Page("situational_reports.py", title = "Situational Reports"),
-    st.Page("manage_users.py", title = "User Management")
+    #st.Page("manage_users.py", title = "User Management")
 ]
 
 pg = st.navigation(pages)
