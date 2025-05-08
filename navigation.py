afd4a257df8b1273a11d72f48658eff395dc6d9e
sachincms <sachin.r@catalysts.org>
2025-05-08 10:34:52 +0530

    changed page file path based on directory

--- navigation.py
+++ navigation.py
@@ -2,8 +2,8 @@ import streamlit as st
 import os
 
 pages = [
-    st.Page(os.path.join("pages","chat_app.py"), title = "Chatbot"),
-    st.Page(os.path.join("pages", "situational_reports.py"), title = "Situational Reports"),
+    st.Page(os.path.join("app_pages","chat_app.py"), title = "Chatbot"),
+    st.Page(os.path.join("app_pages", "situational_reports.py"), title = "Situational Reports"),
 ]
 
 pg = st.navigation(pages)
